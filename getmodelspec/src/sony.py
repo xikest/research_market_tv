5f2be7b2dbe9b800a2d1eafd52c271617c2b8171
xikest <xikest12@gmail.com>
2023-06-05 13:22:46 +0900

    .

--- getmodelspec/src/sony.py
+++ getmodelspec/src/sony.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import pickle
+
 from ..tools.webdriver import WebDriver
 
 
@@ -19,24 +21,31 @@ class GetSONY:
         ## 메인 페이지에서 시리즈를 추출
         seriesUrls = self.getPage1st(url= 'https://electronics.sony.com/tv-video/televisions/c/all-tvs')
 
+        # ==========================================================================
+        self.backUp(seriesUrls, "seriesUrls")
+        # ==========================================================================
+
         ## 서브 시리즈 페이지에서 모델을 추출
-        dict_allSeries = {}
+        dictAllSeries = {}
         for url in seriesUrls:
             dictSeries = self.getPage2nd(url=url)
             print(dictSeries)
-            dict_allSeries.update(dictSeries)
-        print("Number of all Series:", len(dict_allSeries))
+            dictAllSeries.update(dictSeries)
+        print("Number of all Series:", len(dictAllSeries))
 
+
+        # ==========================================================================
+        self.backUp(dictAllSeries, "dictAllSeries")
+        # ==========================================================================
 
         ## 모든 모델 리스트를 추출
         dictModels = {}
-        for model_url in dict_allSeries.values():
+        for model_url in dictAllSeries.values():
             print(model_url)
             try:
                 dictModels.update(self.getPage3rd(model_url))
-            except:
-                print("page error:",model_url)
-            print("dictModels",dictModels)
+            except Exception as e:
+                print(model_url, f"\n getPage3rd error ({e})")
         dfModels = pd.DataFrame.from_dict(dictModels, orient="index")
         return dfModels
 
@@ -45,20 +54,18 @@ class GetSONY:
     def getPage1st(self, url:str) -> set:
         set_mainSeries = set()
         scrolling_cnt: int = 10
-        # pageWaiting:int = 5
 
         wd =  WebDriver.get_crome()
         wd.get(url=url)
-        # wait = WebDriverWait(wd, pageWaiting)
+        self.waitingPage(5)
 
         for i in range(scrolling_cnt):
             time.sleep(1)
-            # elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'custom-product-grid-item__product-name')))
             elements = wd.find_elements(By.CLASS_NAME, value='custom-product-grid-item__product-name')  ## 모든 인치 모델 가져 옴
             for element in elements:
                 try: set_mainSeries.add(element.get_attribute('href')) #URL만 저장 함
-                except:
-                    print('main_page_error')
+                except Exception as e:
+                    print(f"getPage1st error ({e})")
                     pass
             wd.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
         wd.quit()
@@ -71,10 +78,9 @@ class GetSONY:
 
         wd = WebDriver.get_crome()
         wd.get(url=url)
-        wait = WebDriverWait(wd, pageWaiting)
+        self.waitingPage(5)
 
-        seriesName = self.getNamefromURL(url)
-        # time.sleep(5)
+        wait = WebDriverWait(wd, pageWaiting)
         elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'custom-variant-selector__item')))  ## 시리즈의 모든 인치 모델 가져 옴
         # elements = wd.find_elements(By.CLASS_NAME, 'custom-variant-selector__item')  ## 시리즈의 모든 인치 모델 가져 옴
         for element in elements:
@@ -82,36 +88,37 @@ class GetSONY:
                 element_url = element.get_attribute('href')
                 label = self.getNamefromURL(element_url)
                 dictSeries[label]=element_url # url 만 저장 함
-            except:
-                print('sub_page_error')
+            except Exception as e:
+                print(f"getPage2nd error ({e})")
                 pass
         wd.quit()
-        print(f"Number of SONY {seriesName[4:]} series:", len(dictSeries))
+        print(f"Number of SONY {self.getNamefromURL(url)[4:]} series:", len(dictSeries))
         return dictSeries
 
     ###======================final stage===============================##
-    def getPage3rd(self, url:str) -> pd.DataFrame:
-        dictModel = {}
+    def getPage3rd(self, url:str) -> dict:
+        dictSpec = {}
         pageWaiting:int = 10
 
         wd = WebDriver.get_crome()
         wd.get(url=url)
         wait = WebDriverWait(wd, pageWaiting)
+        self.waitingPage(10)
 
         #모델 정보 확인
         model = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'product-intro__code'))).text.replace("Model: ", "")
         descr = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'product-intro__title'))).text.strip()
         # print(descr)
-        dictModel["Year"] = re.findall(r"\((\d{4})\)", descr)
-        dictModel["Size"] = re.findall(r"(\d+\")", descr)
-        dictModel["Descr"] = descr
+        dictSpec["Year"] = re.findall(r"\((\d{4})\)", descr)
+        dictSpec["Size"] = re.findall(r"(\d+\")", descr)
+        dictSpec["Descr"] = descr
 
         #가격 정보 확인
-        dictModel["price"] = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'custom-product-summary__price'))).text
+        dictSpec["price"] = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'custom-product-summary__price'))).text
 
         #이미지 정보 및 url 저장
-        dictModel["src_url"] = url
-        dictModel["Img_url"] = wait.until(EC.presence_of_element_located((By.XPATH, '//app-custom-cx-media//img'))).get_attribute('src')
+        dictSpec["src_url"] = url
+        dictSpec["Img_url"] = wait.until(EC.presence_of_element_located((By.XPATH, '//app-custom-cx-media//img'))).get_attribute('src')
 
         # spec 열기
         wd.find_elements(By.CSS_SELECTOR, ".cx-icon.black_plus")[0].click()
@@ -128,14 +135,14 @@ class GetSONY:
         for i in range(10):
             elements = wd.find_elements(By.CLASS_NAME, "full-specifications__specifications-single-card__sub-list")
             for element in elements:
-                dictModel.update(self.parseTextToDict(element.text.split('\n')))
+                dictSpec.update(self.parseTextToDict(element.text.split('\n')))
                 # print(element.text)
             wd.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
-        # print({model:dictModel})
-
-
         wd.quit()
-        return {model:dictModel}
+
+        dictModel = {model: dictSpec}
+        print(f"{model}\n", dictModel)
+        return dictModel
 
     def getNamefromURL(self, url):
         """
@@ -146,3 +153,17 @@ class GetSONY:
     def parseTextToDict(self, text):
         return {text[i].strip(): text[i + 1].strip() for i in range(0, len(text) - 1, 2)}
 
+    def waitingPage(self, sec=5):
+        time.sleep(sec) # 웹 페이지 대기
+        return None
+
+    def backUp(self, data, filename:str="backup"):
+        # 데이터 저장
+        with open(f"{filename}.pickle", "wb") as file:
+            pickle.dump(data, file)
+
+        # 데이터 불러오기
+        with open(f"{filename}.pickle", "rb") as file:
+            data = pickle.load(file)
+
+        return data
