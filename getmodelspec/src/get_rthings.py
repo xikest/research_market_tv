07141770a5fb32e68d2df1bb7d7d6d57d2c75e01
xikest <94699546+xikest@users.noreply.github.com>
2023-06-08 12:49:13 +0900

    Update get_rthings.py

--- getmodelspec/src/get_rthings.py
+++ getmodelspec/src/get_rthings.py
@@ -1,28 +1,53 @@
 from selenium import webdriver
 
-# Chrome WebDriver 생성
-wd = webdriver.Chrome('chromedriver')
+def get_tv_model_score(prefix, model):
+    """
+    # 함수 호출 예시
+    prefix = "https://www.rtings.com/tv/reviews/sony/"
+    model = "x85k"
+    result = get_tv_model_score(prefix, model)
 
-prefix = "https://www.rtings.com/tv/reviews/sony/x85k"
-model = "x85k"
-url = prefix + model
-
-# 웹 페이지 로드
-wd.get(url)
-
-# scorecard-row-content 클래스를 가진 요소들을 선택
-elements = wd.find_elements_by_css_selector('.scorecard-row-content')
-
-# 각 요소에서 score와 name을 추출하여 출력
-for element in elements:
-    score_element = element.find_element_by_css_selector('.e-score_box-value')
-    name_element = element.find_element_by_css_selector('.scorecard-row-name')
+    # 결과 출력
+    for model, scores in result.items():
+        print(model)
+        for label, score in scores.items():
+            print(label + ": " + score)
+     """
     
-    score = score_element.text.strip()
-    name = name_element.text.strip()
-    print(score)
-    print(name)
+    
+    # Chrome WebDriver 생성
+    wd = webdriver.Chrome('chromedriver')
 
-# WebDriver 종료
-wd.quit()
+    url = prefix + model
+
+    # 웹 페이지 로드
+    wd.get(url)
+
+    # scorecard-row-content 클래스를 가진 요소들을 선택
+    elements = wd.find_elements_by_css_selector('.scorecard-row-content')
+
+    # 결과를 저장할 딕셔너리 초기화
+    scores = {}
+
+    try:
+        for element in elements:
+            score_element = element.find_element(By.css_selector, '.e-score_box-value')
+	label_element = element.find_element(By.css_selector, '.scorecard-row-name')
+
+
+            score = score_element.text.strip()
+            label = label_element.text.strip()
+
+            scores[label] = score
+
+        # WebDriver 종료
+        wd.quit()
+
+        return {model: scores}
+
+    except Exception as e:
+        # WebDriver 종료
+        wd.quit()
+
+        return {model: ""}
 
