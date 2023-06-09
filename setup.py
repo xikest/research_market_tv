a3bd346a555e61aa75ed8ac6adf010b7646df433
xikest <xikest12@gmail.com>
2023-06-09 23:39:08 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='getmodelspec',
-    version='1.0.338',
+    version='1.0.34',
     author='xikest',
     description='get model spec,',
     packages=find_packages(),
