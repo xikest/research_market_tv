ada1868a9b083fb4035bdbc13734b5fd199ea420
xikest <xikest12@gmail.com>
2023-06-09 22:55:56 +0900

    .

--- getmodelspec/tools/webdriver.py
+++ getmodelspec/tools/webdriver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
+import subprocess
 
 class WebDriver:
     def __init__(self):
@@ -8,7 +9,13 @@ class WebDriver:
         pass
 
     @staticmethod
-    def get_crome():
+    def getChrome():
+        """
+        설명:
+            헤드리스 모드로 실행되는 Chrome 웹 드라이버 객체를 반환합니다.
+        반환값:
+            - driver (WebDriver): Chrome 웹 드라이버 객체
+        """
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')  # 헤드리스 모드로 실행
@@ -17,29 +24,26 @@ class WebDriver:
         chrome_options.add_argument('user-agent={0}'.format(user_agent))  # 에이전트 우회
         # chrome_options.add_argument('lang=ko_kr')  # 브라우저 언어
         service = ChromeService(executable_path='chromedriver')
-        wd = webdriver.Chrome(service=service , options=chrome_options)
-        return wd
-
-
-    @staticmethod
-    def get_firefox():
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/60.0'
-        firefox_options = webdriver.FirefoxOptions()
-        firefox_options.add_argument('--headless')  # 헤드리스 모드로 실행
-        firefox_options.add_argument('--no-sandbox')  # 헤드리스 파이어폭스 브라우저를 "사용자 네임스페이스" 옵션 없이 실행하도록 설정
-        firefox_options.add_argument('--disable-dev-shm-usage')  # 헤드리스 파이어폭스 사용시 필요한 설정
-        firefox_options.add_argument('user-agent={0}'.format(user_agent))  # 에이전트 우회
-        wd = webdriver.Firefox(executable_path='geckodriver', options=firefox_options)
-        return wd
+        driver = webdriver.Chrome(service=service , options=chrome_options)
+        return driver
 
     @staticmethod
-    def move_element_to_center(wd:webdriver, element):
+    def move_element_to_center(driver:"WebDriver", element):
+        """
+        설명:
+            주어진 웹 요소(element)를 뷰포트의 중앙으로 이동시킵니다.
+        매개변수:
+            - driver (WebDriver): Selenium 웹 드라이버 객체
+            - element: 움직일 웹 요소
+        반환값:
+            - driver (WebDriver): 이동된 웹 드라이버 객체
+        """
         # 객체로 이동
-        ActionChains(wd).move_to_element(element).perform()
+        ActionChains(driver).move_to_element(element).perform()
 
         # 뷰포트 크기 얻기
-        viewport_width = wd.execute_script("return window.innerWidth;")
-        viewport_height = wd.execute_script("return window.innerHeight;")
+        viewport_width = driver.execute_script("return window.innerWidth;")
+        viewport_height = driver.execute_script("return window.innerHeight;")
 
         # 객체의 위치 얻기
         object_x = element.location['x']
@@ -50,53 +54,67 @@ class WebDriver:
         # 화면 중앙으로 이동
         target_x = object_x + object_width / 2 - viewport_width / 2
         target_y = object_y + object_height / 2 - viewport_height / 2
-        wd.execute_script(f"window.scrollTo({target_x}, {target_y});")
-        return wd
-    #
-    #
-    #
-    # @staticmethod
-    # def move_and_wait_for_element(wd: webdriver, by: str, value: str, timeout: int = 10,
-    #                               expected_conditions: Union[None, str, Tuple] = None) -> webdriver:
-    #     """
-    #     요소를 화면 중앙으로 이동한 후 해당 요소가 불러와질 때까지 대기합니다.
-    #
-    #     :param wd: 웹 드라이버 객체
-    #     :param by: 요소를 찾기 위한 방법 (예: 'id', 'name', 'class_name' 등)
-    #     :param value: 요소를 찾기 위한 값
-    #     :param timeout: 대기 시간 (기본값: 10)
-    #     :param expected_conditions: 요소의 상태를 확인하기 위한 조건 (기본값: None)
-    #                                예: 'visibility_of_element_located', 'element_to_be_clickable' 등
-    #     :return: 웹 드라이버 객체
-    #     """
-    #
-    #     wait = WebDriverWait(wd, timeout)
-    #
-    #     # 요소 찾기
-    #     element = wd.find_element(by, value)
-    #
-    #     # 객체로 이동
-    #     ActionChains(wd).move_to_element(element).perform()
-    #
-    #     # 뷰포트 크기 얻기
-    #     viewport_width = wd.execute_script("return window.innerWidth;")
-    #     viewport_height = wd.execute_script("return window.innerHeight;")
-    #
-    #     # 객체의 위치 얻기
-    #     object_x = element.location['x']
-    #     object_y = element.location['y']
-    #     object_width = element.size['width']
-    #     object_height = element.size['height']
-    #
-    #     # 화면 중앙으로 이동
-    #     target_x = object_x + object_width / 2 - viewport_width / 2
-    #     target_y = object_y + object_height / 2 - viewport_height / 2
-    #     wd.execute_script(f"window.scrollTo({target_x}, {target_y});")
-    #
-    #     if expected_conditions is None:
-    #         element = wait.until(EC.visibility_of_element_located((by, value)))
-    #     else:
-    #         condition_func = getattr(EC, expected_conditions) if isinstance(expected_conditions,
-    #                                                                         str) else expected_conditions
-    #         element = wait.until(condition_func((by, value)))
-    #     return element
+        driver.execute_script(f"window.scrollTo({target_x}, {target_y});")
+        return driver
+
+    @staticmethod
+    def installChromiumSeleniumBs4():
+        """
+        설명:
+            Colab 환경에서 Chromium, Selenium 및 BeautifulSoup4(bs4)를 설치합니다.
+        """
+        script = """
+           # Add debian buster
+           cat > /etc/apt/sources.list.d/debian.list <<'EOF'
+           deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main
+           deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
+           deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
+           EOF
+
+           # Add keys
+           apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
+           apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+           apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
+
+           apt-key export 77E11517 | gpg --dearmour -o /usr/share/keyrings/debian-buster.gpg
+           apt-key export 22F3D138 | gpg --dearmour -o /usr/share/keyrings/debian-buster-updates.gpg
+           apt-key export E562B32A | gpg --dearmour -o /usr/share/keyrings/debian-security-buster.gpg
+
+           # Prefer debian repo for chromium* packages only
+           # Note the double-blank lines between entries
+           cat > /etc/apt/preferences.d/chromium.pref << 'EOF'
+           Package: *
+           Pin: release a=eoan
+           Pin-Priority: 500
+
+
+           Package: *
+           Pin: origin "deb.debian.org"
+           Pin-Priority: 300
+
+
+           Package: chromium*
+           Pin: origin "deb.debian.org"
+           Pin-Priority: 700
+           EOF
+
+           # Install chromium and chromium-driver
+           apt-get update
+           apt-get install chromium chromium-driver
+
+           # Install selenium
+           pip install selenium
+
+           # Install bs4
+           pip install beautifulsoup4
+           
+           # Kill the process after installation
+           import os
+           os.kill(os.getpid(), 9)
+
+       """
+        subprocess.run(['bash', '-c', script], check=True, shell=True)
+
+
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='getmodelspec',
-    version='1.0.3351',
+    version='1.0.337',
     author='xikest',
     description='get model spec,',
     packages=find_packages(),
