941b268a928e3f43f4241de4a1928ccb6a56639f
xikest <xikest12@gmail.com>
2023-11-11 18:57:13 +0900

    .

---
+++ install_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update pip
+pip install --upgrade pip
+
+# Install required Python packages
+pip install -U openpyxl requests selenium beautifulsoup4 tqdm numpy pandas
+
+# Update apt-get
+apt-get update
+
+# Download and setup Chrome
+wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/119.0.6045.105/linux64/chrome-linux64.zip
+unzip chrome-linux64.zip
+rm chrome-linux64.zip
+mv chrome-linux64 chrome
+
+# Download and setup ChromeDriver
+wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/119.0.6045.105/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+rm chromedriver-linux64.zip
+mv chromedriver-linux64 chromedriver
