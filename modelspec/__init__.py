b3987feaf50e237332eccce94737bd20af446bb0
xikest <xikest12@gmail.com>
2023-11-12 09:24:03 +0900

    Update __init__.py

--- modelspec/__init__.py
+++ modelspec/__init__.py
@@ -1,3 +1,2 @@
 from modelspec.model_scraper import ModelScraper as ModelScraper
 from modelspec.tools.filemanager import FileManager as FileManager
-from modelspec.tools.db import ModelInfo as ModelInfo
