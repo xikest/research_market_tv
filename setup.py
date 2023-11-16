5495363f377f9dfad54acb9deb13900f09f66b2d
xikest <xikest12@gmail.com>
2023-11-16 17:40:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='getmodelspec',
-    version='1.342',
+    version='1.343',
     author='xikest',
     description='research TV market',
     packages=find_packages(),
