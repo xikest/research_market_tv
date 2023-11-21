bc1be6a8a1874ca760aca236b4fb2e4a489bc3fa
xikest <94699546+xikest@users.noreply.github.com>
2023-11-21 12:28:11 +0900

    Update __init__.py

--- market_research/scraper/models/__init__.py
+++ market_research/scraper/models/__init__.py
@@ -1,5 +1,5 @@
 from .model_s import ModelScraper_s as SONYSPEC
-from .model_s import ModelScraper_s as DataCleanup_s
-from .model_s import ModelScraper_s as Plotting_s
+from .model_s import DataCleanup_s as DataCleanup_s
+from .model_s import Plotting_s as Plotting_s
 from .model_sjp import ModelScraper_sjp as SONYJPSPEC
 from .model_p import ModelScraper_pjp as PANASONICSPEC
