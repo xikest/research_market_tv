55f76b7072fb4413adf3ca5007cec39bd653bd90
xikest <94699546+xikest@users.noreply.github.com>
2023-11-22 09:14:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkt-retv',
-    version='1.369',
+    version='1.370',
     author='xikest',
     description='market research TV ',
     packages=find_packages(),
