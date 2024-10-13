02b942b249a055094e0f38b501427531a0bfa499
xik <xikest12@gmail..com>
2024-10-13 20:00:13 +0900

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkt-retv',
-    version='1.553',
+    version='1.554',
     author='xikest',
     description='market research TV ',
     packages=find_packages(),
